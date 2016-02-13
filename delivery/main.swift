04f1d46e0362edba28c12696007fc492255d4be3
attheodo <at@atworks.gr>
2016-02-13 02:31:16 +0200

    initial file and simulation parameters parsing

--- delivery/main.swift
+++ delivery/main.swift
@@ -8,5 +8,19 @@
 
 import Foundation
 
-print("Hello, World!")
+let inputFilePath = "/Users/thanosth/Documents/dev/google-hash-2016/specs/busy_day.in"
+
+// Simulation Parameters
+var mapRows: Int = 0
+var mapColumns: Int = 0
+var numOfDrones: Int = 0
+var deadlineTurns: Int = 0
+var maxDronePayload: Int = 0
+
+// parse the text file into an array of lines
+let inputFile = parseInputAtFilePath(inputFilePath)
+
+// parse simulation parameters
+parseSimulationParametersFromInputFile(inputFile)
+
 
