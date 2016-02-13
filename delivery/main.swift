36d2645d1fc3aea6802661c3f6003ac89fc43b34
attheodo <at@atworks.gr>
2016-02-13 22:04:03 +0200

    now parsing products/warehouses/orders, creating service clusters and supplying them appropriately

--- delivery/main.swift
+++ delivery/main.swift
@@ -8,7 +8,10 @@
 
 import Foundation
 
-let inputFilePath = "/Users/thanosth/Documents/dev/google-hash-2016/specs/busy_day.in"
+//let inputFilePath = "/Users/thanosth/Documents/dev/google-hash-2016/specs/busy_day.in"
+//let inputFilePath = "/Users/thanosth/Documents/dev/google-hash-2016/specs/mother_of_all_warehouses.in"
+let inputFilePath = "/Users/thanosth/Documents/dev/google-hash-2016/specs/simple.in"
+//let inputFilePath = "/Users/thanosth/Documents/dev/google-hash-2016/specs/redundancy.in"
 
 // Simulation Parameters
 var mapRows: Int = 0
@@ -17,10 +20,23 @@ var numOfDrones: Int = 0
 var deadlineTurns: Int = 0
 var maxDronePayload: Int = 0
 
+var products: [Product] = []
+var warehouses: [Warehouse] = []
+var orders: [Order] = []
+
 // parse the text file into an array of lines
 let inputFile = parseInputAtFilePath(inputFilePath)
 
 // parse simulation parameters
 parseSimulationParametersFromInputFile(inputFile)
 
+products = parseProductTypesFromInputFile(inputFile)
+warehouses = parseWarehouseDataFromInputFile(inputFile, andProductCatalog: products)
+orders = parseOrderDataFromInputFile(inputFile, andProductCatalog: products)
 
+
+let serviceClusters = serviceClustersBasedOnDistanceForWarehouses(warehouses, andOrders: orders)
+calculateStockForServiceClusters(serviceClusters)
+printServiceClusterStatsForClusters(serviceClusters)
+supplyServiceClusters(serviceClusters)
+printServiceClusterStatsForClusters(serviceClusters)
