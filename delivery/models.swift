193a7b6d42c3187e5ca966df9c8d403c6ea06ad9
attheodo <at@atworks.gr>
2016-02-13 18:42:51 +0200

    Added model for service clusters

--- delivery/models.swift
+++ delivery/models.swift
@@ -10,7 +10,7 @@ import Foundation
 
 typealias Point = (row:Int, column:Int)
 
-struct Product: CustomStringConvertible {
+class Product: CustomStringConvertible, Equatable {
     
     var id: Int
     var weight: Int
@@ -23,12 +23,16 @@ struct Product: CustomStringConvertible {
     }
     
     var description:String {
-        return "ProductId: \(id) (\(weight)kg)"
+        return "📦: 🏷\(id) (\(weight)kg)"
     }
 
 }
 
-struct Warehouse: CustomStringConvertible {
+func ==(lhs: Product, rhs: Product) -> Bool {
+    return lhs.id == rhs.id && rhs.weight == lhs.weight
+}
+
+class Warehouse: CustomStringConvertible {
     
     var id: Int
     var location: Point
@@ -42,27 +46,87 @@ struct Warehouse: CustomStringConvertible {
     }
     
     var description:String {
-        return "WarehouseId: \(id) - Location: [\(location.row),\(location.column)] - # Products: \(inventory.count)"
+        return "🏢: 🏷\(id) - 📍[\(location.row),\(location.column)] - # 📦: \(inventory.count)"
     }
     
 }
 
-struct Order: CustomStringConvertible {
+class Order: CustomStringConvertible {
     
     var id: Int
-    var deliveryLocation: Point
+    var location: Point
     var products: [Product]
     
-    init(id: Int, deliveryLocation location: Point, products: [Product]) {
+    init(id: Int, location: Point, products: [Product]) {
         
         self.id = id
-        self.deliveryLocation = location
+        self.location = location
         self.products = products
     
     }
     
     var description: String {
-        return "OrderId: \(id) - Delivery to: [\(deliveryLocation.row),\(deliveryLocation.column)] - # Products \(products.count)"
+        return "🚩: 🏷\(id) - 📍[\(location.row),\(location.column)] - # 📦 \(products.count)\n"
     }
     
+}
+
+class ServiceCluster: CustomStringConvertible {
+    
+    var id: Int
+    var warehouse: Warehouse
+    var orders: [Order]
+    var surplus: [Product]
+    var deficit: [Product]
+    
+    init(id: Int, warehouse: Warehouse, orders:[Order]) {
+        
+        self.id = id
+        self.warehouse = warehouse
+        self.orders = orders
+        self.surplus = []
+        self.deficit = []
+    
+    }
+    
+    var products: [Product] {
+        return warehouse.inventory
+    }
+    
+    var description: String {
+        return "🌐: \(id) - 🏢: \(warehouse.id) - # 🚩 \(orders.count)"
+    }
+    
+    func assignOrder(order: Order) {
+        self.orders.append(order)
+    }
+    
+    // finds surplus and deficit of products based on the orders assigned
+    func calculateStock() {
+        
+        self.surplus.appendContentsOf(self.products)
+
+        for order in orders {
+
+            let orderDeliverables = order.products
+            
+            for deliverable in orderDeliverables {
+
+                if surplus.contains(deliverable) {
+                    surplus.removeObject(deliverable)
+                } else {
+                    deficit.append(deliverable)
+                }
+            
+            }
+            
+        }
+        
+    }
+    
+    func printStock() {
+        print("🌐: \(id) - 🚩: \(orders.count) - 📦 in 🏢 \(products.count)  - ✅📦: \(surplus.count) - ❌📦: \(deficit.count)")
+    }
+    
+   
 }
