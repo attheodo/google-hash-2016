340b6acd698302ff3ec15251943996f2e51533e7
attheodo <at@atworks.gr>
2016-02-15 21:37:19 +0200

    initial parser logic

---
+++ delivery/delivery/Parser.swift
@@ -0,0 +1,9 @@
+//
+//  Parser.swift
+//  delivery
+//
+//  Created by Athanasios Theodoridis on 15/02/16.
+//  Copyright © 2016 Athanasios Theodoridis. All rights reserved.
+//
+
+import Foundation
