9642fc5c8544d46ace4f6525b2b2fae02bee1701
attheodo <at@atworks.gr>
2016-02-16 03:12:15 +0200

    implemented simulation initialization

--- delivery/delivery/main.swift
+++ delivery/delivery/main.swift
@@ -9,8 +9,18 @@
 import Foundation
 
 //let inputFilePath = "/Users/thanosth/Documents/dev/google-hash-2016/specs/busy_day.in"
-//let inputFilePath = "/Users/thanosth/Documents/dev/google-hash-2016/specs/mother_of_all_warehouses.in"
+let inputFilePath = "/Users/thanosth/Documents/dev/google-hash-2016/specs/mother_of_all_warehouses.in"
 //let inputFilePath = "/Users/thanosth/Documents/dev/google-hash-2016/specs/simple.in"
-let inputFilePath = "/Users/thanosth/Documents/dev/google-hash-2016/specs/redundancy.in"
+//let inputFilePath = "/Users/thanosth/Documents/dev/google-hash-2016/specs/redundancy.in"
+
+startTimer()
 
 let parser = Parser(filePath: inputFilePath)
+
+let simulation = Simulation(products: parser.products, orders: parser.orders, warehouses: parser.warehouses, numOfDrones: parser.numOfDrones, maxDronePayload: parser.maxDronePayload, deadline: parser.deadline, percentageOfDeliveryDrones: 70)
+
+simulation.createServiceClusters()
+simulation.printClusterStats()
+simulation.startSimulation()
+
+stopTimer()
